9856dc6ca98c5bd064c600e9916c8b6b60870054
yugsalvation <33853459+yugsalvation@users.noreply.github.com>
2020-08-25 22:15:41 +0530

    Updates

---
+++ precache-manifest.b64b8e29107b221456a0a56d858551bc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcbcfc7b7718d1ba375f732dcb442fa8",
+    "url": "/shoppies/index.html"
+  },
+  {
+    "revision": "90d036ad1bdba5c1c162",
+    "url": "/shoppies/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "83ccfdb5930b62259a59",
+    "url": "/shoppies/static/css/main.5c04dd5e.chunk.css"
+  },
+  {
+    "revision": "90d036ad1bdba5c1c162",
+    "url": "/shoppies/static/js/2.2923a036.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shoppies/static/js/2.2923a036.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83ccfdb5930b62259a59",
+    "url": "/shoppies/static/js/main.91c6eff9.chunk.js"
+  },
+  {
+    "revision": "27778a2488007f8c9380",
+    "url": "/shoppies/static/js/runtime-main.da667ea0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/shoppies/static/media/logo.5d5d9eef.svg"
+  }
+]);
